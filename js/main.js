bdcb0ba914c9c6b3abaab537cc2e4b4c726d6182
FanatiG <antz96@mail.ru>
2019-10-02 04:02:58 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 'use strict';
 
+var PINSAMOUNT = 8;
+var TITLE = 'title';
+var bungalo = {
+  name: 'bungalo',
+  min: 0,
+  max: 1000000
+};
+var flat = {
+  name: 'flat',
+  min: 1000,
+  max: 1000000
+};
+var house = {
+  name: 'house',
+  min: 5000,
+  max: 1000000
+};
+var palace = {
+  name: 'palace',
+  min: 10000,
+  max: 1000000
+};
+var TYPE = [palace, flat, house, bungalo];
+var ROOMS = [1, 2, 3, 100];
+var GUESTS = ['для 1 гостя', 'для 2 гостей', 'для 3 гостей', 'не для гостей'];
+var CHECKIN = ['12:00', '13:00', '14:00'];
+var CHECKOUT = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var DESCRIPTION = 'description';
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var LOCATIONX = [0, 1150];
+var LOCATIONY = [130, 630];
+
 function getRandomNumber(min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
@@ -22,12 +55,11 @@ function shuffle(arr) {
 }
 
 function randomArr(arr) {
-  var index = Math.floor(Math.random() * Math.floor(arr.length));
-  arr = shuffle(arr).slice(index);
-  return arr;
+  var result = arr;
+  return shuffle(result.slice(Math.floor(Math.random() * Math.floor(arr.length))));
 }
 
-function guestsNumber(roomsNumber, GUESTS) {
+function guestsNumber(roomsNumber) {
   var guestsDescription;
   switch (roomsNumber) {
     case 1:
@@ -37,7 +69,7 @@ function guestsNumber(roomsNumber, GUESTS) {
       guestsDescription = GUESTS[0] + ' ' + GUESTS[1];
       break;
     case 3:
-      guestsDescription = GUESTS[1] + ' ' + GUESTS[2];
+      guestsDescription = GUESTS[0] + ' ' + GUESTS[1] + ' ' + GUESTS[2];
       break;
     case 100:
       guestsDescription = GUESTS[3];
@@ -48,63 +80,59 @@ function guestsNumber(roomsNumber, GUESTS) {
 
 function priceValue(type) {
   var priceDescription;
-  switch (type) {
+  switch (type.name) {
     case 'bungalo':
-      priceDescription = getRandomNumber(0, 1000000);
+      priceDescription = getRandomNumber(type.min, type.max);
       break;
     case 'flat':
-      priceDescription = getRandomNumber(1000, 1000000);
+      priceDescription = getRandomNumber(type.min, type.max);
       break;
     case 'house':
-      priceDescription = getRandomNumber(5000, 1000000);
+      priceDescription = getRandomNumber(type.min, type.max);
       break;
     case 'palace':
-      priceDescription = getRandomNumber(10000, 1000000);
+      priceDescription = getRandomNumber(type.min, type.max);
       break;
   }
   return priceDescription;
 }
 
-function generateDomElements(objArray) {
-  for (var i = 0; i < 8; i++) {
-    var fragment = document.createDocumentFragment();
-    fragment.cloneNode(true);
-    var pin = document.querySelector('#pin').content.querySelector('.map__pin');
-    document.getElementsByClassName('map')[0].classList.remove('map--faded');
+function renderPinsOnMap(pinsList) {
+  var fragment = document.createDocumentFragment();
+  var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+  document.getElementsByClassName('map')[0].classList.remove('map--faded');
+  for (var i = 0; i < PINSAMOUNT; i++) {
     var pinClone = pin.cloneNode(true);
-    pinClone.style.left = objArray[i].location.x + 'px';
-    pinClone.style.top = objArray[i].location.y + 'px';
-    pinClone.querySelector('img').src = objArray[i].author.avatar;
-    pinClone.querySelector('img').alt = objArray[i].offer.title;
+    pinClone.style.left = pinsList[i].location.x + 'px';
+    pinClone.style.top = pinsList[i].location.y + 'px';
+    pinClone.querySelector('img').src = pinsList[i].author.avatar;
+    pinClone.querySelector('img').alt = pinsList[i].offer.title;
     fragment.appendChild(pinClone);
-    document.querySelector('.map__pins').appendChild(fragment);
   }
+  document.querySelector('.map__pins').appendChild(fragment);
 }
 
-function generateArray() {
-  var objArray = [];
-  var TITLE = 'title ' + i;
-  var ADDRESS = '600, 350';
-  var TYPE = ['palace', 'flat', 'house', 'bungalo'];
-  var ROOMS = [1, 2, 3, 100];
-  var GUESTS = ['для 1 гостя', 'для 2 гостей', 'для 3 гостей', 'не для гостей'];
-  var CHECKIN = ['12:00', '13:00', '14:00'];
-  var CHECKOUT = ['12:00', '13:00', '14:00'];
-  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var DESCRIPTION = 'description';
-  var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  var LOCATIONX = [0, 1150];
-  var LOCATIONY = [130, 630];
-  for (var i = 1; i < 9; i++) {
+function generatePinData() {
+  var pinsList = [];
+  for (var i = 1; i < PINSAMOUNT + 1; i++) {
+    var rndRooms = randomArrElement(ROOMS);
+    var rndGuests = guestsNumber(rndRooms, GUESTS);
+    var rndType = randomArrElement(TYPE);
+    var rndPrice = priceValue(rndType);
+    var pinCoordX = getRandomNumber(LOCATIONX[0], LOCATIONX[1]);
+    var pinCoordY = getRandomNumber(LOCATIONY[0], LOCATIONY[1]);
+    var address = pinCoordX + ' ' + pinCoordY;
     var obj = {
       'author': {
         'avatar': './img/avatars/user0' + i + '.png'
       },
       'offer': {
         'title': TITLE,
-        'address': ADDRESS,
-        'type': randomArrElement(TYPE),
-        'rooms': randomArrElement(ROOMS),
+        'address': address,
+        'type': rndType.name,
+        'rooms': rndRooms,
+        'price': rndPrice,
+        'guests': rndGuests,
         'checkin': randomArrElement(CHECKIN),
         'checkout': randomArrElement(CHECKOUT),
         'features': randomArr(FEATURES),
@@ -112,14 +140,12 @@ function generateArray() {
         'photos': randomArr(PHOTOS)
       },
       'location': {
-        'x': getRandomNumber(LOCATIONX[0], LOCATIONX[1]),
-        'y': getRandomNumber(LOCATIONY[0], LOCATIONY[1]),
+        'x': pinCoordX,
+        'y': pinCoordY,
       }
     };
-    obj.offer.price = priceValue(obj.offer.type);
-    obj.offer.guests = guestsNumber(obj.offer.rooms, GUESTS);
-    objArray.push(obj);
+    pinsList.push(obj);
   }
-  generateDomElements(objArray);
+  renderPinsOnMap(pinsList);
 }
-generateArray();
+generatePinData();
