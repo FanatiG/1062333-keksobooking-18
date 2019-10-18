349af8d40b47f3c151f56100388e2c2519d42241
FanatiG <antz96@mail.ru>
2019-10-19 02:16:40 +0300

    5-3

--- js/pin.js
+++ js/pin.js
@@ -24,8 +24,55 @@
     mapPinsElement.appendChild(fragment);
   }
 
-  function mainPinMouseDown() {
-    activatePage();
+  function mainPinMouseDown(evt) {
+    if (formHtmlClassList.contains('ad-form--disabled')) {
+      activatePage();
+    } else {
+      dragDropMainPin(evt);
+    }
+  }
+
+  function dragDropMainPin(evt) {
+    var mainPin = window.data.mainPinElement;
+    mainPin.style.zIndex = getComputedStyle(document.querySelectorAll('.map__pin:not(.map__pin--main)')[0])['zIndex'] + 1;
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var mainPinMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+      if (mainPin.style.left.slice(0, -2) < window.data.locationX[0]) {
+        mainPin.style.left = window.data.locationX[0] + 'px';
+      }
+      if (mainPin.style.left.slice(0, -2) > window.data.locationX[1]) {
+        mainPin.style.left = window.data.locationX[1] + 'px';
+      }
+      if (mainPin.style.top.slice(0, -2) < window.data.locationY[0]) {
+        mainPin.style.top = window.data.locationY[0] + 'px';
+      }
+      if (mainPin.style.top.slice(0, -2) > window.data.locationY[1]) {
+        mainPin.style.top = window.data.locationY[1] + 'px';
+      }
+    };
+    var mainPinMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      mainPin.removeEventListener('mousemove', mainPinMouseMove);
+      mainPin.removeEventListener('mouseup', mainPinMouseUp);
+      window.data.setAddressValue(window.data.PIN_LEG_HEIGHT);
+    };
+    mainPin.addEventListener('mousemove', mainPinMouseMove);
+    mainPin.addEventListener('mouseup', mainPinMouseUp);
   }
 
   function mainPinEnterDown(evt) {
@@ -57,10 +104,9 @@
     window.data.mainPinElement.addEventListener('mousedown', mainPinMouseDown);
     window.data.mainPinElement.addEventListener('keydown', mainPinEnterDown);
   }();
+
   function activatePage() {
     formHtmlClassList.remove('ad-form--disabled');
-    window.data.mainPinElement.removeEventListener('mousedown', mainPinMouseDown);
-    window.data.mainPinElement.removeEventListener('keydown', mainPinEnterDown);
     window.data.toggleAvailability(window.data.menuFieldsetElementList, false);
     window.data.setAddressValue(window.data.PIN_LEG_HEIGHT);
     window.data.activateMap();
